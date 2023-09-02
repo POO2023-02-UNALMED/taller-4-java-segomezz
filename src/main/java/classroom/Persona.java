b7a88ae6e4bb82a51c8874a1ff2d269f274734bf
segomezz <141938389+segomezz@users.noreply.github.com>
2023-09-02 17:24:51 -0500

    Update Persona.java

--- src/main/java/classroom/Persona.java
+++ src/main/java/classroom/Persona.java
@@ -34,7 +34,7 @@ public class Persona {
     }
 
     public Persona(String nombre) {
-        this.nombre = "";
+        this.nombre =nombre;
         this.cedula=1;
         totalPersonas++;
     }
